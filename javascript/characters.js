89c8a67c3415575d27d4e490d8ada3c74368c48d
Isak Jönsson <isak.j1819@gmail.com>
2026-03-12 09:23:00 +0100

    ändrade fetch & renderJedi
    ändrade så fetch och renderjedi är separata functioner

--- javascript/characters.js
+++ javascript/characters.js
@@ -1,80 +1,66 @@
+const akababUrl = "https://raw.githubusercontent.com/akabab/starwars-api/master/api/all.json";
+const dataBankUrl = "https://starwars-databank-server.vercel.app/api/v1/characters?page=2&limit=total"
 const jediCard = document.querySelector(".shell-card")
 
-//fetch star wars data from both akabab and star wars data bank api
-async function fetchStarWarsData() {
-    const akababUrl = "https://raw.githubusercontent.com/akabab/starwars-api/master/api/all.json";
-    const dataBankUrl = "https://starwars-databank-server.vercel.app/api/v1/characters?page=2&limit=total"
 
+ async function fetchAndCombineSWData() {
 
-    try {
+    //  fetch the data paralell with eachother for faster loading
+    const [akababRes, dataBankRes] = await Promise.all([
+        fetch(akababUrl).then(res => res.json()),
+        fetch(dataBankUrl).then(res => res.json())
 
-        //  fetch the data paralell with eachother for faster loading
-        const [akababRes, dataBankRes] = await Promise.all([
-            fetch(akababUrl),
-            fetch(dataBankUrl)
+    ])
 
-        ])
+    const combineData = akababRes.map(char => {
+        const dataBankCharacters = dataBankRes.data
 
-        const akababData = await akababRes.json()
-        const dataBankData = await dataBankRes.json()
-        const dataBankCharacters = dataBankData.data
+        const matchNames = dataBankCharacters.find(dbChar => dbChar.name === char.name)
 
-        // combine data
-        const combineData = akababData.map(char => {
-            // find matching Names from both apis 
-            const matchNames = dataBankCharacters.find(dbChar => dbChar.name === char.name)
-
-            const newData = {
-                name: char.name,
-                image: char.image,
-                affiliations: char.affiliations,
-                description: matchNames ? matchNames.description : "No description found"
-            }
-
-            return newData
-        })
-        console.log('Combined data:', combineData)
-        // find sith
-        const sith = combineData.filter(char => char.affiliations?.includes("Sith"));
-        console.log("Hittade sith:", sith)
-
-        // find Jedi
-        const jedi = combineData.filter(char => char.affiliations?.includes("Jedi Order"));
-        console.log("Hittade jedi:", jedi)
-
-        const renderJedi = async () => {
-            console.log(jediCard)
-            jedi.forEach(j => {
-                const jediDiv = document.createElement('div')
-                const charImgEl = document.createElement('img')
-                const charNameEl = document.createElement('h1')
-                const charDesEl = document.createElement('p')
-
-                const name = j.name
-                const description = j.description
-                const charImg = j.image
-               
-                   charImgEl.classList.add('charImg')
-                   jediDiv.classList.add('JediDiv')
-
-                charImgEl.src = charImg
-                charNameEl.innerText = `${name}`
-                charDesEl.innerText = `Description: ${description}`
-
-
-                jediDiv.append(charImgEl,charNameEl, charDesEl)
-                jediCard.append(jediDiv)
-
-            });
+        const newData = {
+            name: char.name,
+            image: char.image,
+            affiliations: char.affiliations,
+            description: matchNames ? matchNames.description : "No description found"
         }
-        renderJedi()
 
-    } catch (err) {
-        console.error('does not work')
+        return newData
+    })
+    console.log('Combined data:', combineData)
 
+    const jedi = combineData.filter(char => char.affiliations?.includes("Jedi Order"));
+    console.log("Hittade jedi:", jedi)
+    renderJedi(jedi)
 
-    }
+    const sith = combineData.filter(char => char.affiliations?.includes("Sith"));
+    console.log("Hittade sith:", sith)
+
 }
 
-fetchStarWarsData()
+const renderJedi = async (jedi) => {
+   
+    jedi.forEach(j => {
+        const jediDiv = document.createElement('div')
+        const charImgEl = document.createElement('img')
+        const charNameEl = document.createElement('h1')
+        const charDesEl = document.createElement('p')
 
+        const name = j.name
+        const description = j.description
+        const charImg = j.image
+
+        charImgEl.classList.add('charImg')
+        jediDiv.classList.add('JediDiv')
+
+        charImgEl.src = charImg
+        charNameEl.innerText = name
+        charDesEl.innerText = `Description: ${description}`
+
+
+        jediDiv.append(charImgEl, charNameEl, charDesEl)
+        jediCard.append(jediDiv)
+
+    });
+}
+
+fetchAndCombineSWData()
