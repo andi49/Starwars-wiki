c33db862b609e5a2390244154614e5895538b549
Charlie <challe-1992@hotmail.com>
2026-03-17 11:23:11 +0100

    xwing sound added

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,8 +1,15 @@
-const menuBtn = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.site-nav');
+const menuBtn = document.querySelector('.menu-toggle')
+const nav = document.querySelector('.site-nav')
 const imgStarfighter = document.querySelector('.imgStarfighter')
+const audioBasePath = window.location.pathname.toLowerCase().includes('/src/')
+  ? '../audio/'
+  : 'audio/'
+
+const clicksound = new Audio(`${audioBasePath}tiefightershot.mp3`)
+clicksound.preload = 'auto'
 
 menuBtn.addEventListener('click', () => {
-  nav.classList.toggle("active");
-  imgStarfighter.classList.toggle('active');
-});
+  nav.classList.toggle('active')
+  imgStarfighter.classList.toggle('active')
+  clicksound.play().catch(() => {})
+})
